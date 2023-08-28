384950ef2caed2d040a3cea6be52934819724f1e
miguedorta7 <75344292+miguedorta7@users.noreply.github.com>
2023-08-28 08:34:13 -0300

    Redux (#7)
    * maquetado cities
    
    * sprint2-front
    
    * vistas de formulario

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,37 @@
-
-
 export default function SignIn() {
   return (
-    <div>Signin under construction</div>
-  )
+    <div>
+      <main className="main-form">
+        <article>
+          <h1 className="h1-form">My Tinerary</h1>
+        </article>
+        <aside className="father-form">
+          <form className="container-form">
+            <h6 className="step-form">step 1 of 2</h6>
+            <h3 className="create-text">Sign in</h3>
+            <p>New user? <a href=""> Create an account</a></p>
+            <div className="row mb-3 email-form">
+              <div className="col-sm-10">
+                <input
+                  type="email"
+                  name="mail"
+                  className="form-control"
+                  id="mail"
+                  placeholder="Email"
+                />
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary btn-form">
+              Continue
+            </button>
+            <hr className="hr" />
+            <div className="d-grid gap-3 col-9 my-5 mx-4 container-gf">
+              <button className="btn btn-primary rounded-pill continue-g" type="button"><img src="img/2000px-Google_G_Logo.svg_.png" className="boton-google"  alt="btn google" />Continue with google </button>
+              <button className="btn btn-primary rounded-pill continue-f" type="button"><img src="img/facebook-white.png" className="boton-fb" alt="btn-fb" />Continue with facebook </button>
+            </div>
+          </form>
+        </aside>
+      </main>
+    </div>
+  );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,35 @@
 
 export default function SignUp() {
   return (
-    <div>Signup under construction</div>
+    <div>
+      <main className="main-form">
+        <article>
+          <h1 className="h1-form">My Tinerary</h1>
+        </article>
+        <aside className="father-form">
+          <form className="container-form">
+            <h6 className="step-form">step 2 of 2</h6>
+            <h3 className="create-text">Enter your password</h3>
+            
+            <div className="row mb-3 password-form">
+              <div className="col-sm-10">
+                <input type="password" name="password" className="form-control input-email" id="password" placeholder="Password" autoComplete="on"/>
+              </div>
+            </div>
+          
+            <button type="submit" className="btn btn-primary btn-form">
+              Continue
+            </button>
+            <hr className="hr" />
+            <div className="d-grid gap-3 col-9 my-5 mx-4 container-gf">
+              <button className="btn btn-primary rounded-pill continue-g" type="button"><img src="img/2000px-Google_G_Logo.svg_.png" className="boton-google"  alt="btn google" />Continue with google </button>
+              <button className="btn btn-primary rounded-pill continue-f" type="button"><img src="img/facebook-white.png" className="boton-fb" alt="btn-fb" />Continue with facebook </button>
+            </div>
+      
+            
+          </form>
+        </aside>
+      </main>
+    </div>
   )
 }
